9215182a0e5887194fc125eb3dff41872dafb8cc
‘NateMietk’ <natemietkiewicz@gmail.com>
2017-11-02 16:59:09 +0000

    updates and first commits

--- src/3_data_prep.R
+++ src/3_data_prep.R
@@ -8,7 +8,9 @@ library(lubridate)
 library(tidyverse)
 library(gridExtra)
 
-source("src/R/helper_functions.R")
+source("src/functions/st_par.R")
+ncores <- detectCores()/2
+
 # Set directories and projections -----------------------------------------
 
 #EPSG:2163 USA_Contiguous_Albers_Equal_Area_Conic
@@ -18,24 +20,20 @@ proj_ea <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96+x_0=0 +y_0=
 proj_ed <- "+proj=eqdc +lat_0=39 +lon_0=-96 +lat_1=33 +lat_2=45 +x_0=0+y_0=0 +datum=NAD83 +units=m +no_defs"
 
 # Clean the USA States layer ---------------------------------------------
-usa_shp <- st_read(dsn = "../data/bounds/state",
+usa <- st_read(dsn = file.path(prefix, "bounds/state"),
                    layer = "cb_2016_us_state_20m", quiet= TRUE) %>%
   st_transform("+init=epsg:2163") %>%  # e.g. US National Atlas Equal Area
   filter(!(NAME %in% c("Alaska", "Hawaii", "Puerto Rico"))) %>%
-  mutate(area_m2 = as.numeric(st_area(geometry)),
-         StArea_km2 = area_m2/1000000,
-         group = 1) %>%
   st_simplify(., preserveTopology = TRUE) 
 
 # Dissolve to the USA Boundary
-conus <- usa_shp %>%
-  group_by(group) %>%
+conus <- usa %>%
   st_union()
 
-# Clean the FPA database class ---------------------------------------------
-shrt_fire <- st_read(dsn = "../data/fire/Short_9215/Data/FPA_FOD_20170508.gdb",
-                     layer = "Fires", quiet= FALSE) %>%
-  filter(!(STATE %in% c("Alaska", "Hawaii", "Puerto Rico")) & FIRE_SIZE >= 0.1) %>%
+# Clean the FPA database class
+fpa_fire <- st_read(dsn = file.path(fpa_prefix, "Data", "FPA_FOD_20170508.gdb"),
+                    layer = "Fires", quiet= FALSE) %>%
+  filter(!(STATE %in% c("Alaska", "Hawaii", "Puerto Rico") & FIRE_SIZE >= 0.1)) %>%
   dplyr::select(FPA_ID, LATITUDE, LONGITUDE, ICS_209_INCIDENT_NUMBER, ICS_209_NAME, MTBS_ID, MTBS_FIRE_NAME,
                 FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, STAT_CAUSE_DESCR, FIRE_SIZE, STATE) %>%
   mutate(IGNITION = ifelse(STAT_CAUSE_DESCR == "Lightning", "Lightning", "Human"),
@@ -44,10 +42,11 @@ shrt_fire <- st_read(dsn = "../data/fire/Short_9215/Data/FPA_FOD_20170508.gdb",
          FIRE_SIZE_ha = FIRE_SIZE_m2*10000,
          DISCOVERY_DAY = day(DISCOVERY_DATE),
          DISCOVERY_MONTH = month(DISCOVERY_DATE),
-         DISCOVERY_YEAR = FIRE_YEAR) 
-shrt_fire <- st_transform(shrt_fire, "+init=epsg:2163")
+         DISCOVERY_YEAR = FIRE_YEAR)  %>%
+  st_make_valid()
+fpa_fire <- st_transform(fpa_fire, "+init=epsg:2163")
 
-st_write(shrt_fire, "../data/fire/Short_9215/gpkg/short_clean_conus.gpkg", 
+st_write(shrt_fire, "../data/fire/Short_9215/gpkg/short_clean_conus.gpkg",
          driver = "GPKG",
          update=TRUE)
 
@@ -68,24 +67,33 @@ st_write(landowner, "../data/bounds/public_private_lands/gpkg/pad_conus.gpkg",
          driver = "GPKG",
          update=TRUE)
 
-# Clean the WUI -------------------------------------
-
-wui <- st_read(dsn = paste0("../data/anthro/", "us_wui_2010.gdb"),
+# Import the Wildland-Urban Interface and process
+wui <- st_read(dsn = file.path(wui_prefix, "us_wui_2010.gdb"),
                layer = "us_wui_2010") %>%
   st_simplify(., preserveTopology = TRUE, dTolerance = 0.001) %>%
   mutate(Class = classify_wui(WUICLASS10)) %>%
-  filter(Class == "WUI" | Class == "VLD" | Class == "Wildlands") %>%
-  st_transform("+init=epsg:2163")
+  filter(Class %in% c("Urban" ,"WUI", "VLD", "Wildlands")) %>%
+  st_transform("+init=epsg:2163") %>%
+  st_make_valid()
 
-
-st_write(wui, normalizePath("../data/anthro/gpkg/wui_us.gpkg"), 
+st_write(wui, file.path(prefix, "anthro/gpkg/wui_us.gpkg"), 
          driver = "GPKG",
          update=TRUE)
 
+# Intersect FPA with WUI
+fpa_wui <- fpa_fire %>%
+  st_intersection(., wui) %>%
+  st_make_valid()
+
+if (!file.exists(file.path(fpa_out, "fpa_wui_conus.gpkg"))) {
+  st_write(fpa_wui, file.path(fpa_out, "fpa_wui_conus.gpkg"),
+           driver = "GPKG",
+           update=TRUE)}
+
 # Intersect FPA data with PAD-US -------------------------------------
 
-fire_landowner <- st_intersection(shrt_fire, landowner)
-
-st_write(fire_landowner, "../data/bounds/public_private_lands/gpkg/shrt_pad_us.gpkg",
+fire_landowner <- st_par(fpa_wui, st_intersection, n_cores = ncores, y = landowner)
+  
+st_write(fire_landowner, file.path(prefix, "bounds/public_private_lands/gpkg/shrt_pad_us.gpkg"),
          driver = "GPKG")
 #
